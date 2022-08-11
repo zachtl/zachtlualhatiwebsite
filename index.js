cceb9f7ad542f6428cae38b24aff3633055099f4
Zach Lualhati <81999933+zachtl@users.noreply.github.com>
2022-08-11 22:07:18 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        html, body {
+            height: 100%;
+            overflow-x: hidden;
+            overflow-y: hidden;
+        }
+    </style>
+    <title>Document</title>
+    <!-- Instagram meta tags -->
+    <meta property="og:title" content="Uhh, i havent finished this site" />
+    <meta property="og:type" content="video.other" />
+    <meta property="og:url" content="https://www.zachlualhati.com" />
+    <meta property="og:image" content="https://i.pinimg.com/564x/54/70/d8/5470d831c3e36581db6022a0fb3dfd17.jpg" />
+</head>
+<body>
+    <img style="display: block;margin: auto;background-color: hsl(0, 0%, 90%);height:100vh; width: 100vw" src="https://c.tenor.com/L64tU6Z5hNYAAAAC/pug-dog.gif">
+</body>
+</html>
