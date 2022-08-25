3366ed30c28e61b457b013f9af504c1c1f5f6f68
zachtl <zachtlualhati@gmail.com>
2022-08-25 17:55:13 +1000

    💯

--- src/containers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Footer } from './footer/Footer';
-export { default as ZachLualhati } from './ZachLualhati/ZachLualhati';
-export { default as WhoAmI } from './WhoAmI/WhoAmI';
-export { default as Experiences } from './Experiences/Experiences';
-export { default as Resume } from './Resume/Resume';
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import './index.css';
-
-// createRoot(document.GetElementById('root')).render(<App />);
-
-ReactDOM.render(<App />, document.getElementById('root'));
