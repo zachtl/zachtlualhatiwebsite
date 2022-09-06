8cf682e989e3889e29516558dc6bf4dfe3d16282
zachtl <zachtlualhati@gmail.com>
2022-09-06 20:22:28 +1000

    project1

---
+++ portfolio/CatsWorldDomination/index.js
@@ -0,0 +1,20 @@
+window.addEventListener("scroll", function (event) {
+    let scroll = this.scrollY;
+    if (((~window.innerHeight + 100) + scroll) > 0) {
+        document.querySelector("nav").classList.add("navfil");
+    } else {
+        document.querySelector("nav").classList.remove("navfil");
+    }
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("main1");
+    var xvalue = "center";
+    var factor = 0.75;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+    document.querySelector("div.prog").style = "width: "+((scroll*100)/(document.documentElement.scrollHeight - document.documentElement.clientHeight))+"%";
+    if ((scroll*100)/(document.documentElement.scrollHeight - document.documentElement.clientHeight) == 100) {
+        document.querySelector("nav").classList.add("navfin");
+    } else {
+        document.querySelector("nav").classList.remove("navfin");
+    }
+})
