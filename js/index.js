b20e2e194ab8cc20b844fe9d16f4fd1ad330c199
Zach Lualhati <zachtlualhati@gmail.com>
2022-09-07 19:59:16 +1000

    update
    :)

--- js/index.js
+++ js/index.js
@@ -1,18 +1,62 @@
-var settings = {
-    "url": "https://catfact.ninja/fact",
-    "method": "GET",
-    "timeout": 0
+if ((navigator.userAgent.indexOf("Chrome") > -1) && (navigator.userAgent.indexOf("Safari") > -1)) {
+    window.addEventListener("scroll", function (event) {
+        let scroll = this.scrollY;
+        if (((~window.innerHeight + 100) + scroll) > 0) {
+            document.querySelector("nav").classList.add("navfil");
+        } else {
+            document.querySelector("nav").classList.remove("navfil");
+        }
+        var scrolltotop = document.scrollingElement.scrollTop;
+        var target = document.getElementById("main1");
+        var xvalue = "center";
+        var factor = 0.75;
+        var yvalue = scrolltotop * factor;
+        target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+        document.querySelector("div.prog").style = "width: " + ((scroll * 100) / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) + "%";
+    })
+} else {
 }
 
-$.ajax(settings).done(function (resp1) {
-  console.log(resp1.fact);
-  document.getElementById("res").innerText = resp1.fact;
-  $.ajax({
-    "url": "https://api.thecatapi.com/v1/images/search",
-    "method": "GET",
-    "timeout": 0
-    }).done(function (resp2){
-        console.log(resp2[0].url)
-        document.body.style.backgroundImage = 'url("'+ resp2[0].url +'")';
-    })
-});
+var txt = [
+    'print("Hello!")',
+    'std::cout<<"Hello!";',
+    'Console.WriteLine("Hello!");',
+    'echo "Hello!";',
+    'System.out.println("Hello!");',
+    'console.log("Hello!")',
+]
+
+var i = x = 0
+
+type()
+
+function deleteType() {
+    if (document.getElementById("type").innerText.length != 0) {
+        document.getElementById("type").innerText = document.getElementById("type").innerText.slice(0, -1);
+        if (document.getElementById("type").innerText == "") {
+            document.getElementById("type").innerText = " ";
+        }
+        setTimeout(deleteType, 50);
+    } else {
+        if (x == 5) {
+            x = 0
+        } else {
+            x++;
+        }
+        i = 0;
+        type()
+    }
+}
+
+function type() {
+    if (i < txt[x].length) {
+        if (document.getElementById("type").innerText == " ") {
+            document.getElementById("type").innerText = document.getElementById("type").innerText.slice(0, -1);
+        }
+        document.getElementById("type").innerText += txt[x].charAt(i)
+        i++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(deleteType, 2500);
+    }
+}
